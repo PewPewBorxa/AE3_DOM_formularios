0d62e1b223d4298eac4cc483cd3d4ebc12350668
botolo <datborx@gmail.com>
2020-11-02 20:18:44 +0100

    AE3 <Finalizacion>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,172 +1,9 @@
 document.title = 'AE3_DOM_FORMULARIOS';
-/* 
 
-const div_paso_1 = document.createElement('div');
-div_paso_1.id = 'paso_1';
-div_paso_1.className = 'paso_1';
-
-const h1_p1 = document.createElement('h1');
-h1_p1.appendChild(document.createTextNode('AE3 DOM Formularios'));
-const p_p1 = document.createElement('p');
-p_p1.appendChild(document.createTextNode('Introduce una url:'));
-const form_p1 = document.createElement('form');
-
-const l_url = document.createElement('label');
-l_url.setAttribute('for', 'url');
-l_url.appendChild(document.createTextNode('URL:'));
-const url = document.createElement('input');
-url.setAttribute('type', 'url');
-url.setAttribute('name', 'callbackurl');
-url.id = 'url';
-url.required = true;
-url.setAttribute('onchange','setURL()');
-
-div_paso_1.appendChild(h1_p1);
-div_paso_1.appendChild(p_p1);
-div_paso_1.appendChild(l_url);
-div_paso_1.appendChild(url);
-
-const div_form_i = document.createElement('div');
-div_form_i.id = 'formulario_solicitud_datos';
-div_form_i.className = 'formulario_solicitud_datos';
-
-const form_i = document.createElement('form');
-form_i.setAttribute('action', 'index.html');
-
-const l_name = document.createElement('label');
-l_name.setAttribute('for', 'name');
-l_name.appendChild(document.createTextNode('Nombre:'));
-
-const name = document.createElement('input');
-name.setAttribute('type', 'text');
-name.setAttribute('name', 'name');
-name.id = 'name';
-name.required = true;
-
-const l_surname = document.createElement('label');
-l_surname.setAttribute('for', 'surname');
-l_surname.appendChild(document.createTextNode('Apellidos:'));
-
-const surname = document.createElement('input');
-surname.setAttribute('type', 'text');
-surname.setAttribute('pattern', '.+ .');
-surname.setAttribute('name', 'surname');
-surname.id = 'surname';
-surname.required = true;
-
-const l_bday = document.createElement('label');
-l_bday.setAttribute('for', 'birth');
-l_bday.appendChild(document.createTextNode('Fecha de nacimiento:'));
-
-const bday = document.createElement('input');
-bday.setAttribute('type', 'date');
-bday.setAttribute('name', 'birth');
-bday.id = 'birth';
-bday.required = true;
-
-const l_dir = document.createElement('label');
-l_dir.setAttribute('for', 'direccion');
-l_dir.appendChild(document.createTextNode('Dirección:'));
-
-const dir = document.createElement('input');
-dir.setAttribute('type', 'text');
-dir.setAttribute('name', 'direccion');
-dir.id = 'direccion';
-
-const l_post = document.createElement('label');
-l_post.setAttribute('for', 'postcode');
-l_post.appendChild(document.createTextNode('Código postal:'));
-
-const post = document.createElement('input');
-post.setAttribute('type', 'number');
-post.setAttribute('name', 'postcode');
-post.setAttribute('maxlength', 5);
-post.setAttribute('size', 5);
-post.setAttribute('minlength', 5);
-post.required = true;
-post.id = 'postcode';
-
-const l_prov = document.createElement('label');
-l_prov.setAttribute('for', 'provincia');
-l_prov.appendChild(document.createTextNode('Provincia:'));
-
-const prov = document.createElement('select');
-prov.setAttribute('name', 'provincia');
-prov.required = true;
-prov.id = 'provincia';
-
-const op0 = document.createElement('option');
-op0.setAttribute('value', '');
-op0.disabled = true;
-op0.selected = true;
-op0.appendChild(document.createTextNode('Escoge tu provincia'));
-
-const op1 = document.createElement('option');
-op1.setAttribute('value', 'valencia');
-op1.appendChild(document.createTextNode('Valencia'));
-
-const op2 = document.createElement('option');
-op2.setAttribute('value', 'Alicante');
-op2.appendChild(document.createTextNode('Alicante'));
-
-const op3 = document.createElement('option');
-op3.setAttribute('value', 'castellon');
-op3.appendChild(document.createTextNode('Castellón'));
-
-prov.appendChild(op0);
-prov.appendChild(op1);
-prov.appendChild(op2);
-prov.appendChild(op3);
-
-const l_mun = document.createElement('label');
-l_mun.setAttribute('for', 'municipio');
-l_mun.appendChild(document.createTextNode('Municipio:'));
-
-const mun = document.createElement('input');
-mun.setAttribute('type', 'text')
-mun.setAttribute('name', 'municipio');
-mun.required = true;
-mun.id = 'municipio';
-
-const reset = document.createElement('input');
-reset.setAttribute('type', 'reset')
-reset.setAttribute('value', 'Borrar');
-reset.id = 'reset';
-reset.className = 'botones_form';
-
-const submit = document.createElement('input');
-submit.setAttribute('type', 'submit')
-submit.setAttribute('value', 'Enviar');
-submit.id = 'submit';
-submit.className = 'botones_form';
-
-
-
-div_form_i.appendChild(form_i);
-form_i.appendChild(l_name);
-form_i.appendChild(name);
-form_i.appendChild(l_surname);
-form_i.appendChild(surname);
-form_i.appendChild(l_bday);
-form_i.appendChild(bday);
-form_i.appendChild(l_dir);
-form_i.appendChild(dir);
-form_i.appendChild(l_post);
-form_i.appendChild(post);
-form_i.appendChild(l_prov);
-form_i.appendChild(prov);
-form_i.appendChild(l_mun);
-form_i.appendChild(mun);
-form_i.appendChild(reset);
-form_i.appendChild(submit);
-
- */
-/* PASO 3 */
-
-const name = document.getElementById('name');
+const Nombre = document.getElementById('name');
 const surname = document.getElementById('surname');
 const bday = document.getElementById('birth');
-const dir = document.getElementById('direccion');
+const direccion = document.getElementById('direccion');
 const post = document.getElementById('postcode');
 const prov = document.getElementById('provincia');
 const mun = document.getElementById('municipio');
@@ -185,13 +22,13 @@ var indice = 1;
 var CallbackUrl = '';
 
 const continuar = document.getElementById('Continue');
-const atras = document.getElementById('BackButton');
 continuar.setAttribute('onclick', 'next()');
+const atras = document.getElementById('BackButton');
 atras.setAttribute('onclick', 'back()');
 
 function setURL() {
     CallbackUrl = url.value;
-    FormValidation();
+    FormValidation(document.getElementById('form_paso_1'));
 }
 
 const div_form_i = document.getElementById('formulario_solicitud_datos');
@@ -199,25 +36,54 @@ const div_paso_1 = document.getElementById('paso_1');
 document.body.removeChild(div_form_i);
 document.body.appendChild(div_paso_1);
 
+const AceptaDatos = document.createElement('button');
+AceptaDatos.id = 'submit_datos';
+AceptaDatos.className = 'botones_form';
+AceptaDatos.disabled = false;
+AceptaDatos.appendChild(document.createTextNode('SI'));
+AceptaDatos.setAttribute('onclick', 'AceptarDatos()');
+div_review.appendChild(AceptaDatos);
+
+const Agradecimiento = document.createElement('div');
+const PAgradecimiento = document.createElement('p');
+PAgradecimiento.appendChild(document.createTextNode('Gracias por rellenar el formulario, \n Pulsa Terminar para ir a la URL que pusiste en el primer paso.'));
+Agradecimiento.appendChild(PAgradecimiento);
+
+const Terminar = document.createElement('button');
+Terminar.id = 'finish';
+Terminar.className = 'botones_form';
+Terminar.disabled = false;
+Terminar.appendChild(document.createTextNode('Terminar'));
+Terminar.setAttribute('onclick', 'EndSurvey()');
+Agradecimiento.appendChild(Terminar);
+
 function next() {
     switch (indice + 1) {
         case 2:
-            
+            continuar.disabled = true;
+            document.getElementById('BackButton').classList.remove("hidden");
             document.body.removeChild(div_paso_1);
             document.body.appendChild(div_form_i);
             indice = indice + 1;
-            document.getElementById('indice').value = indice;
+
             break;
 
         case 3:
-            
+            document.getElementById('BackButton').classList.remove("hidden");
+            continuar.disabled = true;
             document.body.removeChild(div_form_i);
             document.body.appendChild(div_review);
             indice = indice + 1;
-            document.getElementById('indice').value = indice;
+
             break;
 
         case 4:
+            document.body.removeChild(div_review);
+            document.body.appendChild(Agradecimiento);
+            continuar.disabled = true;
+            continuar.className = '' + continuar.className + ' hidden';
+            indice = indice + 1;
+
 
             break;
         default:
@@ -228,17 +94,16 @@ var datos;
 const json = document.createElement('pre');
 json.id = json;
 
-
 function formSubmit() {
     event.preventDefault();
     datos = FormToJson();
-    console.log(datos);
     json.textContent = datos;
     appendJSON();
+    document.getElementById('Continue').disabled = false;
 }
 
 function FormToJson() {
-    let data = new FormData(document.getElementById('form_1'));
+    let data = new FormData(document.getElementById('form_sol_datos'));
     let JSONdata = JSON.stringify(Object.fromEntries(data), null, 2);
     return JSONdata;
 }
@@ -250,6 +115,7 @@ function appendJSON() {
     console.log(keys);
     console.log(data);
     let pre = document.createElement('pre');
+    pre.id = 'pre';
     for (let i = 0; i < keys.length; i++) {
         let aux = keys[i];
         pre.appendChild(document.createTextNode(keys[i] + ' :: ' + data[aux] + '\n'));
@@ -261,20 +127,24 @@ function appendJSON() {
 function back() {
     switch (indice - 1) {
         case 2:
+            continuar.disabled = false;
+            div_review.removeChild(document.getElementById('pre'));
             document.body.removeChild(div_review);
+            document.body.removeChild(AceptaDatos);
             document.body.appendChild(div_form_i);
             indice = indice - 1;
             break;
 
         case 3:
-            document.getElementById('indice').value = indice;
-            document.body.removeChild();
+            continuar.disabled = false;
+            document.body.removeChild(Agradecimiento);
             document.body.appendChild(div_review);
             indice = indice - 1;
             break;
 
         case 1:
-            document.getElementById('indice').value = indice;
+            document.getElementById('BackButton').className = '' + document.getElementById('BackButton').className + ' hidden';
+            continuar.disabled = false;
             document.body.removeChild(div_form_i);
             document.body.appendChild(div_paso_1);
             indice = indice - 1;
@@ -286,18 +156,56 @@ function back() {
 function HideElement() {
     if (!(indice > 1)) {
         document.getElementById('BackButton').className = 'botones hidden';
-    }else {
+    } else {
         document.getElementById('BackButton').className = 'botones show';
     }
 }
 
-function FormValidation() {
-    const form = document.getElementById('form_paso_1');
+function FormValidation(form) {
     for (i = 0; i < form.length; i++) {
         if (form[i].checkValidity()) {
+            form[i].classList.remove("errores");
             document.getElementById('Continue').disabled = false;
         } else {
             form[i].className = '' + form[i].className + ' errores';
+            document.getElementById('Continue').disabled = true;
         }
     }
+}
+
+function ValidarFormulario2(element) {
+    x = element;
+    if (x.checkValidity()) {
+        x.classList.remove("errores");
+    } else {
+        x.className = '' + x.className + ' errores';
+    }
+    ValdiaSubmit();
+}
+
+function ValdiaSubmit() {
+    const form = document.getElementById('form_sol_datos');
+    yes = false;
+    for (i = 0; i < form.length; i++) {
+        if (form[i].checkValidity() == true) {
+            yes = true;
+        } else {
+            yes = false;
+            break;
+        }
+    }
+    if (yes) {
+        document.getElementById('submit').disabled = false;
+    } else {
+        document.getElementById('submit').disabled = true;
+        document.getElementById('Continue').disabled = true;
+    }
+}
+
+function AceptarDatos() {
+    document.getElementById('Continue').disabled = false;
+}
+
+function EndSurvey() {
+    window.location.replace(CallbackUrl);
 }
